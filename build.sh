053197164cec3bb1f0266cb662fba8155d98f5d7
Ahmet Erer <ahmet@Ahmet-MacBook-Air-2.local>
2025-01-03 23:57:56 +0300

    Build script basitleştirildi

--- build.sh
+++ build.sh
@@ -5,19 +5,6 @@ set -e
 
 echo "Starting build process..."
 
-# Environment setup
-echo "Setting up environment..."
-export NODE_VERSION=18.17.0
-export PYTHON_VERSION=3.11.0
-
-# Install or update Node.js
-echo "Setting up Node.js $NODE_VERSION..."
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-nvm install $NODE_VERSION
-nvm use $NODE_VERSION
-
 # Frontend build
 echo "Building frontend..."
 cd web
@@ -48,13 +35,6 @@ cd ..
 # Backend setup
 echo "Setting up backend..."
 cd backend
-
-# Setup Python virtual environment
-echo "Setting up Python virtual environment..."
-python -m pip install --upgrade pip
-pip install virtualenv
-python -m virtualenv venv
-source venv/bin/activate
 
 echo "Installing Python packages..."
 pip install -r requirements.txt
